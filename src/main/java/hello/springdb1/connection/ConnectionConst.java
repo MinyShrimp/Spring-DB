c054b9b37b47fecf312759f610c1d3b634a67839
MinyShrimp <ksk7584@gmail.com>
2023-02-23 19:28:48 +0900

    1강: JDBC 이해 - 데이터베이스 연결

---
+++ src/main/java/hello/springdb1/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.springdb1.connection;
+
+public interface ConnectionConst {
+    String URL = "jdbc:h2:tcp://localhost/~/test";
+    String USERNAME = "sa";
+    String PASSWORD = "";
+}
