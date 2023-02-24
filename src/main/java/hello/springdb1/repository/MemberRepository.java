c182ec27b1a4c1181b0c00f4e5bbce2649ccf5d1
MinyShrimp <ksk7584@gmail.com>
2023-02-24 22:21:01 +0900

    6강: 스프링과 문제 해결 - 예외 처리, 반복 - 체크 예외와 인터페이스

---
+++ src/main/java/hello/springdb1/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.springdb1.repository;
+
+import hello.springdb1.domain.Member;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Member findById(String memberId);
+
+    void update(String memberId, int money);
+
+    void delete(String memberId);
+}
